219cea6b08d7ab9c7278d0f385a124bd90968b70
Vikrant <vikrantmalik051@gmail.com>
2019-04-11 03:42:31 +0530

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'users/new'
+  root 'static_pages#home'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'users#showUsers'
+  resources :users
+end
